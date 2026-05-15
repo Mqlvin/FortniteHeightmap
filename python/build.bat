9edafce9eaf482f22e1113fdf8d858bce20b08f4
Mqlvin <pricklydotcactus@gmail.com>
2026-05-15 12:02:06 +0100

    update v1.1, modify py exe filename

--- python/build.bat
+++ python/build.bat
@@ -1,17 +1,17 @@
 REM this batch thing is so wacky
 
 @echo off
-if exist export_server.exe del /f /q export_server.exe
+if exist export_server.exe del /f /q export-server.exe
 
-pyinstaller --onefile --name export_server .\export_server.py
+pyinstaller --onefile --name export-server .\export_server.py
 if errorlevel 1 (
   echo PyInstaller failed
   exit /b 1
 )
 
-if exist export_server.spec del /f /q export_server.spec
+if exist export-server.spec del /f /q export-server.spec
 
-if exist .\dist\export_server.exe copy /y .\dist\export_server.exe .\
+if exist .\dist\export-server.exe copy /y .\dist\export-server.exe .\
 
 if exist dist rmdir /s /q dist
 if exist build rmdir /s /q build
