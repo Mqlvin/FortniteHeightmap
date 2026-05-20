c6f00cd75eac72b45ab95c473920f5c3077c2560
Mqlvin <pricklydotcactus@gmail.com>
2026-05-20 11:39:37 +0100

    remove console window from release profile, windows

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 use eframe::egui;
 use egui::{Color32, Ui};
 use crate::heightmap::{error::GenerationError, generate_heightmap};
